14257221f4ec4e8cd06a94d7e1900f9a5415ffcb
pengmao <pengmao@yunqilaohe.com>
2024-05-18 18:25:51 +0800

    feat: 增加demo内容

--- example/e-one/kubeFrontend/src/App.tsx
+++ example/e-one/kubeFrontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Tabs } from 'antd'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -15,6 +16,7 @@ function App() {
   }
   return (
     <>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
